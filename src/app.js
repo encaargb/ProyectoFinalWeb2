7835093ce13e04a57fcbdbc4635a1214f6877931
Jesus <jesusjoanaazuara@gmail.com>
2026-04-02 18:44:32 +0200

    Añadido Swagger UI para visualizar OpenAPI

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 const installationsRoutes = require('./routes/installations.routes');
 const sportsRoutes = require('./routes/sports.routes');
 const weatherRoutes = require('./routes/weather.routes');
-
+const swaggerDocument = YAML.load('./docs/openapi.yaml');
 const app = express();
 
 app.use(express.json());
+
 app.get('/', (req, res) => {
     res.json({ message: 'Sports Facilities API is running' });
 });
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/installations', installationsRoutes);
 app.use('/sports', sportsRoutes);
 app.use('/weather-records', weatherRoutes);
 
 module.exports = app;
+
 
