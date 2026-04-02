95950f0fc5b875195ae9686a3f55debc914589f6
Jesus <jesusjoanaazuara@gmail.com>
2026-04-02 18:24:15 +0200

    Añadido openApi y el esqueleto de rutas

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
-const app = express();
-app.use(express.json());
+const installationsRoutes = require('./routes/installations.routes');
+const sportsRoutes = require('./routes/sports.routes');
+const weatherRoutes = require('./routes/weather.routes');
 
+const app = express();
+
+app.use(express.json());
 app.get('/', (req, res) => {
     res.json({ message: 'Sports Facilities API is running' });
 });
+
+app.use('/installations', installationsRoutes);
+app.use('/sports', sportsRoutes);
+app.use('/weather-records', weatherRoutes);
+
 module.exports = app;
+
